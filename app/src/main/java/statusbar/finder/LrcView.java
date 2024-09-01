4b117b05f091f338fd01c5baeddfe42e02299a0f
EX3124 <70867298+EX3124@users.noreply.github.com>
2024-09-01 22:57:45 +0800

    歌词延迟调整
    点击通知打开调整界面,调整歌词的出现时机(如+100ms会使歌词更快出现)

---
+++ app/src/main/java/statusbar/finder/LrcView.java
@@ -0,0 +1,49 @@
+package statusbar.finder;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.fragment.app.FragmentActivity;
+
+
+public class LrcView extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lrcview);
+
+        TextView lrc =findViewById(R.id.lrc);
+        String lrc1 = MusicListenerService.instance.getLyric().sentenceList.toString();
+        String lrc2 = lrc1.substring(1, lrc1.length() -1);
+        String lrc3 = lrc2.replaceAll("\\d+: ", "");
+        String lrc4 = lrc3.replaceAll(", ","\n");
+        lrc.setText(lrc4);
+
+        TextView offset =findViewById(R.id.offset);
+        Button plus =findViewById(R.id.plus);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MusicListenerService.instance.setLyricOffset(MusicListenerService.instance.getLyric().offset - 100);
+                offset.setText("offset: " + String.valueOf((MusicListenerService.instance.getLyric().offset > 0) ? -MusicListenerService.instance.getLyric().offset : Math.abs(MusicListenerService.instance.getLyric().offset)));
+            }
+        });
+
+        Button minus =findViewById(R.id.minus);
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MusicListenerService.instance.setLyricOffset(MusicListenerService.instance.getLyric().offset + 100);
+                offset.setText("offset: " + String.valueOf((MusicListenerService.instance.getLyric().offset > 0) ? -MusicListenerService.instance.getLyric().offset : Math.abs(MusicListenerService.instance.getLyric().offset)));
+            }
+        });
+
+
+
+    }
+
+}
